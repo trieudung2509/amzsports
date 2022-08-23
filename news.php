bf760ff25ca35eeec249db31b0979ecd2225d39e
DangKhoa96 <khoanguyenptit96@gmail.com>
2022-08-24 01:40:23 +0700

    create news page

---
+++ news.php
@@ -0,0 +1,108 @@
+<?php include "templates/head.php"; ?>
+
+<body>
+  <?php include "templates/header.php"; ?>
+  <div class="news-page-wrapper">
+    <div class="container">
+      <h3 class="title">News</h3>
+      <div class="list-news">
+        <div class="news-item">
+          <img src="./images/news-image.png" alt="" class="news-image" />
+          <div class="news-info">
+            <span class="league-name">Premier League</span>
+            <h3 class="news-title">City hit four to see off bournemouth and make it two from two dngid kd gnfdhigfn knfid</h3>
+            <span class="time">10 hours ago</span>
+          </div>
+        </div>
+        <div class="news-item">
+          <img src="./images/news-image-2.png" alt="" class="news-image" />
+          <div class="news-info">
+            <span class="league-name">Premier League</span>
+            <h3 class="news-title">City hit four to see off bournemouth and make it two from two dngid kd gnfdhigfn knfid</h3>
+            <span class="time">10 hours ago</span>
+          </div>
+        </div>
+        <div class="news-item">
+          <img src="./images/news-image-3.png" alt="" class="news-image" />
+          <div class="news-info">
+            <span class="league-name">Premier League</span>
+            <h3 class="news-title">City hit four to see off bournemouth and make it two from two dngid kd gnfdhigfn knfid</h3>
+            <span class="time">10 hours ago</span>
+          </div>
+        </div>
+        <div class="news-item">
+          <img src="./images/news-image-4.png" alt="" class="news-image" />
+          <div class="news-info">
+            <span class="league-name">Premier League</span>
+            <h3 class="news-title">City hit four to see off bournemouth and make it two from two dngid kd gnfdhigfn knfid</h3>
+            <span class="time">10 hours ago</span>
+          </div>
+        </div>
+        <div class="news-item">
+          <img src="./images/news-image-5.png" alt="" class="news-image" />
+          <div class="news-info">
+            <span class="league-name">Premier League</span>
+            <h3 class="news-title">City hit four to see off bournemouth and make it two from two dngid kd gnfdhigfn knfid</h3>
+            <span class="time">10 hours ago</span>
+          </div>
+        </div>
+        <div class="news-item">
+          <img src="./images/news-image-6.png" alt="" class="news-image" />
+          <div class="news-info">
+            <span class="league-name">Premier League</span>
+            <h3 class="news-title">City hit four to see off bournemouth and make it two from two dngid kd gnfdhigfn knfid</h3>
+            <span class="time">10 hours ago</span>
+          </div>
+        </div>
+        <div class="news-item">
+          <img src="./images/news-image-3.png" alt="" class="news-image" />
+          <div class="news-info">
+            <span class="league-name">Premier League</span>
+            <h3 class="news-title">City hit four to see off bournemouth and make it two from two dngid kd gnfdhigfn knfid</h3>
+            <span class="time">10 hours ago</span>
+          </div>
+        </div>
+        <div class="news-item">
+          <img src="./images/news-image-4.png" alt="" class="news-image" />
+          <div class="news-info">
+            <span class="league-name">Premier League</span>
+            <h3 class="news-title">City hit four to see off bournemouth and make it two from two dngid kd gnfdhigfn knfid</h3>
+            <span class="time">10 hours ago</span>
+          </div>
+        </div>
+        <div class="news-item">
+          <img src="./images/news-image-5.png" alt="" class="news-image" />
+          <div class="news-info">
+            <span class="league-name">Premier League</span>
+            <h3 class="news-title">City hit four to see off bournemouth and make it two from two dngid kd gnfdhigfn knfid</h3>
+            <span class="time">10 hours ago</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="pagination">
+        <button class="page-item prev-btn disabled">
+          <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M4.67299 2.037C4.59103 2.12829 4.55005 2.22862 4.55005 2.338C4.55005 2.44737 4.59103 2.54304 4.67299 2.625L8.93861 7L4.67299 11.375C4.59103 11.457 4.55005 11.5526 4.55005 11.662C4.55005 11.7714 4.59103 11.867 4.67299 11.949C4.75494 12.031 4.85061 12.0719 4.95999 12.0719C5.06936 12.0719 5.16503 12.0355 5.24699 11.9626L9.77249 7.30056C9.85444 7.2186 9.89542 7.11827 9.89542 6.99956C9.89542 6.88085 9.85444 6.78052 9.77249 6.69856L5.24699 2.03656C5.16503 1.96364 5.06936 1.92719 4.95999 1.92719C4.85061 1.92719 4.75494 1.96364 4.67299 2.03656V2.037Z" fill="#1A202C"/>
+          </svg>
+        </button>
+        <button class="page-item active">1</button>
+        <button class="page-item">2</button>
+        <button class="page-item">3</button>
+        <button class="page-item">4</button>
+        <button class="page-item">5</button>
+        <button class="page-item">6</button>
+        <button class="page-item">...</button>
+        <button class="page-item">100</button>
+        <button class="page-item next-btn">
+          <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M4.67299 2.037C4.59103 2.12829 4.55005 2.22862 4.55005 2.338C4.55005 2.44737 4.59103 2.54304 4.67299 2.625L8.93861 7L4.67299 11.375C4.59103 11.457 4.55005 11.5526 4.55005 11.662C4.55005 11.7714 4.59103 11.867 4.67299 11.949C4.75494 12.031 4.85061 12.0719 4.95999 12.0719C5.06936 12.0719 5.16503 12.0355 5.24699 11.9626L9.77249 7.30056C9.85444 7.2186 9.89542 7.11827 9.89542 6.99956C9.89542 6.88085 9.85444 6.78052 9.77249 6.69856L5.24699 2.03656C5.16503 1.96364 5.06936 1.92719 4.95999 1.92719C4.85061 1.92719 4.75494 1.96364 4.67299 2.03656V2.037Z" fill="#1A202C"/>
+          </svg>
+        </button>
+      </div>
+    </div>
+  </div>
+  <?php include "templates/footer.php"; ?>
+</body>
+
+</html>
--- templates/footer.php
+++ templates/footer.php
@@ -10,6 +10,7 @@
 
 <!-- JAVASCRIPT-->
 <script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/jquery-ui.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
 <script src="js/main.js"></script>
