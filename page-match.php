76058f98f8ced8b339268c2d0fdb979a188517df
Lucas Mario <lucas.brightsoft@gmail.com>
2022-08-24 17:15:58 +0700

    page detail match

---
+++ page-match.php
@@ -0,0 +1,350 @@
+<?php include "templates/head.php"; ?>
+<body>
+	<?php include "templates/header.php"; ?>
+  <div class="match-body">
+      <div class="container">
+        <div class="row">
+            <div class="match-header">
+                <h5>Liverpool - Chelsea</h5>
+                <div class="match-time">
+                    <p>10/08/2022 02:00</p>
+                    <div class="match-social">
+                        <ul class="social-ul">
+                            <li>
+                                <a href="">
+                                  <img src="/images/facebook.png" alt="">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                  <img src="/images/logo-twitter.png" alt="">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                  <img src="/images/curved-next.png" alt="">
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="block-play">
+              <div class="match-play">
+                  <img src="/images/match.png" alt="">
+                  <div class="match-server">
+                      <ul class="server-list">
+                        <li class="active">
+                          <a href="" class="btn btn-server">Server 1</a>
+                        </li>
+                        <li>
+                          <a href="" class="btn btn-server">Server 2</a>
+                        </li>
+                        <li>
+                          <a href="" class="btn btn-server">Server 3</a>
+                        </li>
+                      </ul>
+                  </div>
+              </div>
+            </div>
+            <div class="block-result">
+                <div class="item-result">
+                    <div class="bottom-premier-item">
+                      <span>Liverpool</span>
+                      <img src="/images/mu.png" alt="">
+                    </div>
+                    <div class="bottom-point">
+                        <span>5 : 0</span>
+                    </div>
+                    <div class="bottom-premier-item1">
+                      <img src="/images/mu.png" alt="">
+                      <span>Liverpool</span>
+                    </div>
+                </div>
+                <div class="item-date">
+                    <p>10/08/2022 02:00</p>
+                </div>
+                <div class="item-last">
+                    <p>Finished</p>
+                </div>
+            </div>
+        </div>
+        <div class="row" style="padding: 10px 0px">
+            <div class="col-md-2 col-match">
+                <div class="list-guide">
+                    <img src="/images/clock.png" alt="">
+                    <h6>Sunday, 14/08/2022 00:00</h6>
+                    <p>Match day (GMT+7)</p>
+                </div>
+            </div>
+            <div class="col-md-2 col-match">
+                <div class="list-guide">
+                    <img src="/images/whistle.png" alt="">
+                    <h6>David Gálvez</h6>
+                    <p>Referee</p>
+                </div>
+            </div>
+            <div class="col-md-2 col-match">
+                <div class="list-guide">
+                    <img src="/images/soccer-field.png" alt="">
+                    <h6>Regular Season - 1</h6>
+                    <p>Round</p>
+                </div>
+            </div>
+            <div class="col-md-2 col-match">
+                <div class="list-guide">
+                    <img src="/images/calendar-2.png" alt="">
+                    <h6>2022</h6>
+                    <p>Season</p>
+                </div>
+            </div>
+            <div class="col-md-2 col-match">
+                <div class="list-guide">
+                    <img src="/images/stadium.png" alt="">
+                    <h6>Estadio Municipal de Anduva</h6>
+                    <p>Stadium</p>
+                </div>
+            </div>
+            <div class="col-md-2 col-match">
+                <div class="list-guide">
+                    <img src="/images/new-construction.png" alt="">
+                    <h6>Miranda de Ebro</h6>
+                    <p>City</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+          <div class="head-to-head">
+              <h4>Head to Head</h4>
+              <div class="block-head">
+                  <table>
+                    <tbody>
+                      <tr>
+                        <td>
+                            <div class="total-win right">
+                                <span class="loading" style="width: 10%" ></span>
+                            </div>
+                        </td>
+                        <td>
+                          <span>11</span>
+                        </td>
+                        <td>
+                          <span>
+                            Total Wins
+                          </span>
+                        </td>
+                        <td rowspan="3" class="play-center" >
+                          <div class="play-td">
+                            <span>Played</span>
+                            <p>28</p>
+                            <span class="draws">Draws: 9</span>
+                          </div>
+                        </td>
+                        <td>
+                          <span>
+                            Total Wins
+                          </span>
+                        </td>
+                        <td>
+                          <span>
+                            Total Wins
+                          </span>
+                        </td>
+                        <td>
+                          11
+                        </td>
+                        <td>
+                            <div class="total-win left">
+                                <span class="loading" style="width: 20%"></span>
+                            </div>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                            <div class="total-win right">
+                                <span class="loading" style="width: 30%"></span>
+                            </div>
+                        </td>
+                        <td>
+                          <span>11</span>
+                        </td>
+                        <td>
+                          <span>
+                            Total Wins
+                          </span>
+                        </td>
+                        <td>
+                          <span>
+                            Total Wins
+                          </span>
+                        </td>
+                        <td>
+                          <span>
+                            Total Wins
+                          </span>
+                        </td>
+                        <td>
+                          11
+                        </td>
+                        <td>
+                            <div class="total-win left">
+                                <span class="loading" style="width: 10%"></span>
+                            </div>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                            <div class="total-win right">
+                                <span class="loading" style="width: 50%"></span>
+                            </div>
+                        </td>
+                        <td>
+                          <span>11</span>
+                        </td>
+                        <td>
+                          <span>
+                            Total Wins
+                          </span>
+                        </td>
+                        <td>
+                          <span>
+                            Total Wins
+                          </span>
+                        </td>
+                        <td>
+                          <span>
+                            Total Wins
+                          </span>
+                        </td>
+                        <td>
+                          11
+                        </td>
+                        <td>
+                            <div class="total-win left">
+                                <span class="loading" style="width: 53%"></span>
+                            </div>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+              </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="recent-meeting">
+            <h4>Recent Meetings</h4>
+              <table class="table">
+                <tbody class="tbody-match">
+                  <tr>
+                    <td>
+                      <div class="td-first">
+                        <span>Saturday 15 May 2004</span>
+                        <div class="bottom-premier-item">
+                          <span>Liverpool</span>
+                            <img src="/images/mu.png" alt="">
+                        </div>
+                      </div>
+                    </td>
+                    <th>
+                      <span>5</span> : <span>0</span>
+                    </th>
+                    <td>
+                      <div class="td-last">
+                        <div class="bottom-premier-item">
+                            <span>Chelsea</span>
+                            <img src="/images/mu.png" alt="">
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <div class="td-first">
+                        <span>Saturday 15 May 2004</span>
+                        <div class="bottom-premier-item">
+                          <span>Liverpool</span>
+                            <img src="/images/mu.png" alt="">
+                        </div>
+                      </div>
+                    </td>
+                    <th>
+                      <span>5</span> : <span>0</span>
+                    </th>
+                    <td>
+                      <div class="td-last">
+                        <div class="bottom-premier-item">
+                            <span>Chelsea</span>
+                            <img src="/images/mu.png" alt="">
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <div class="td-first">
+                        <span>Saturday 15 May 2004</span>
+                        <div class="bottom-premier-item">
+                          <span>Liverpool</span>
+                            <img src="/images/mu.png" alt="">
+                        </div>
+                      </div>
+                    </td>
+                    <th>
+                      <span>5</span> : <span>0</span>
+                    </th>
+                    <td>
+                      <div class="td-last">
+                        <div class="bottom-premier-item">
+                            <span>Chelsea</span>
+                            <img src="/images/mu.png" alt="">
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <div class="td-first">
+                        <span>Saturday 15 May 2004</span>
+                        <div class="bottom-premier-item">
+                          <span>Liverpool</span>
+                            <img src="/images/mu.png" alt="">
+                        </div>
+                      </div>
+                    </td>
+                    <th>
+                      <span>5</span> : <span>0</span>
+                    </th>
+                    <td>
+                      <div class="td-last">
+                        <div class="bottom-premier-item">
+                            <span>Chelsea</span>
+                            <img src="/images/mu.png" alt="">
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+          </div>
+        </div>
+        <div class="row">
+            <div class="match-news">
+                <div class="item-news">
+                    <h5>Overal Information</h5>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+                </div>
+                <div class="item-news">
+                    <h5>Overal Information</h5>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+                </div>
+                <div class="item-news">
+                    <h5>Overal Information</h5>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+                </div>
+            </div>
+        </div>
+      </div>
+  </div>
+	<?php include "templates/footer.php"; ?>
+</body>
+</html>
