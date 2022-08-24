7483c6be2ea43bf6f368c4192e12666f6511505d
Lucas Mario <lucas.brightsoft@gmail.com>
2022-08-24 20:51:51 +0700

    feat: page football ui

---
+++ page-football.php
@@ -0,0 +1,375 @@
+<?php include "templates/head.php"; ?>
+<body>
+	<?php include "templates/header.php"; ?>
+
+  <div class="football-body">
+    <div class="container">
+        <div class="row">
+          <div class="football-header">
+            <div class="bottom-premier">
+                <img src="/images/premier-league.png" alt="">
+                Premier League
+            </div>
+          </div>
+          <div class="foot-desc">
+              <p>Premier League fixtures on TV are shown on Sky Sports, BT Sport and Amazon Prime Video with 200 games shown live each season. Sky Sports show 128 Premier League TV fixtures, BT Sport show 52 matches and Amazon Prime show 20 live games.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="foot-table">
+                <ul class="foot-ul">
+                    <li>
+                    		<table class="table">
+													<thead>
+														<tr>
+															<td colspan="2" class="p-0">
+																	<div class="header-title">
+																		<h6>Sunday 14th August 2022</h6>
+																	</div>
+															</td>
+														</tr>
+													</thead>
+													<tbody class="tbody-match">
+														<tr>
+															<td>02:00</td>
+															<td>Finished</td>
+															<td>
+																<div class="bottom-premier">
+																		<img src="/images/premier-league.png" alt="">
+																		Premier League
+																</div>
+															</td>
+															<td>
+																<div class="bottom-premier-item">
+																	<span>Liverpool</span>
+																		<img src="/images/mu.png" alt="">
+																</div>
+															</td>
+															<th>
+																5 : 0
+															</th>
+															<td>
+																<div class="bottom-premier-item">
+																		<span>Chelsea</span>
+																		<img src="/images/mu.png" alt="">
+																</div>
+															</td>
+														</tr>
+														<tr class="live">
+															<td>02:00</td>
+															<td>Live</td>
+															<td>
+																<div class="bottom-premier">
+																		<img src="/images/premier-league.png" alt="">
+																		Premier League
+																</div>
+															</td>
+															<td>
+																<div class="bottom-premier-item">
+																	<span>Liverpool</span>
+																		<img src="/images/mu.png" alt="">
+																</div>
+															</td>
+															<th>
+																5 : 0
+															</th>
+															<td>
+																<div class="bottom-premier-item">
+																		<span>Chelsea</span>
+																		<img src="/images/mu.png" alt="">
+																</div>
+															</td>
+														</tr>
+														<tr class="live">
+															<td>02:00</td>
+															<td>Live</td>
+															<td>
+																<div class="bottom-premier">
+																		<img src="/images/premier-league.png" alt="">
+																		Premier League
+																</div>
+															</td>
+															<td>
+																<div class="bottom-premier-item">
+																	<span>Liverpool</span>
+																		<img src="/images/mu.png" alt="">
+																</div>
+															</td>
+															<th>
+																5 : 0
+															</th>
+															<td>
+																<div class="bottom-premier-item">
+																		<span>Chelsea</span>
+																		<img src="/images/mu.png" alt="">
+																</div>
+															</td>
+														</tr>
+														<tr class="upcoming">
+															<td>02:00</td>
+															<td>Upcoming</td>
+															<td>
+																<div class="bottom-premier">
+																		<img src="/images/premier-league.png" alt="">
+																		Premier League
+																</div>
+															</td>
+															<td>
+																<div class="bottom-premier-item">
+																	<span>Liverpool</span>
+																		<img src="/images/mu.png" alt="">
+																</div>
+															</td>
+															<th>
+																5 : 0
+															</th>
+															<td>
+																<div class="bottom-premier-item">
+																		<span>Chelsea</span>
+																		<img src="/images/mu.png" alt="">
+																</div>
+															</td>
+														</tr>
+													</tbody>
+												</table>
+                    </li>
+										<li>
+                    		<table class="table">
+													<thead>
+														<tr>
+															<td colspan="2" class="p-0">
+																	<div class="header-title">
+																		<h6>Sunday 14th August 2022</h6>
+																	</div>
+															</td>
+														</tr>
+													</thead>
+													<tbody class="tbody-match">
+														<tr>
+															<td>02:00</td>
+															<td>Finished</td>
+															<td>
+																<div class="bottom-premier">
+																		<img src="/images/premier-league.png" alt="">
+																		Premier League
+																</div>
+															</td>
+															<td>
+																<div class="bottom-premier-item">
+																	<span>Liverpool</span>
+																		<img src="/images/mu.png" alt="">
+																</div>
+															</td>
+															<th>
+																5 : 0
+															</th>
+															<td>
+																<div class="bottom-premier-item">
+																		<span>Chelsea</span>
+																		<img src="/images/mu.png" alt="">
+																</div>
+															</td>
+														</tr>
+														<tr class="live">
+															<td>02:00</td>
+															<td>Live</td>
+															<td>
+																<div class="bottom-premier">
+																		<img src="/images/premier-league.png" alt="">
+																		Premier League
+																</div>
+															</td>
+															<td>
+																<div class="bottom-premier-item">
+																	<span>Liverpool</span>
+																		<img src="/images/mu.png" alt="">
+																</div>
+															</td>
+															<th>
+																5 : 0
+															</th>
+															<td>
+																<div class="bottom-premier-item">
+																		<span>Chelsea</span>
+																		<img src="/images/mu.png" alt="">
+																</div>
+															</td>
+														</tr>
+														<tr class="live">
+															<td>02:00</td>
+															<td>Live</td>
+															<td>
+																<div class="bottom-premier">
+																		<img src="/images/premier-league.png" alt="">
+																		Premier League
+																</div>
+															</td>
+															<td>
+																<div class="bottom-premier-item">
+																	<span>Liverpool</span>
+																		<img src="/images/mu.png" alt="">
+																</div>
+															</td>
+															<th>
+																5 : 0
+															</th>
+															<td>
+																<div class="bottom-premier-item">
+																		<span>Chelsea</span>
+																		<img src="/images/mu.png" alt="">
+																</div>
+															</td>
+														</tr>
+														<tr class="upcoming">
+															<td>02:00</td>
+															<td>Upcoming</td>
+															<td>
+																<div class="bottom-premier">
+																		<img src="/images/premier-league.png" alt="">
+																		Premier League
+																</div>
+															</td>
+															<td>
+																<div class="bottom-premier-item">
+																	<span>Liverpool</span>
+																		<img src="/images/mu.png" alt="">
+																</div>
+															</td>
+															<th>
+																5 : 0
+															</th>
+															<td>
+																<div class="bottom-premier-item">
+																		<span>Chelsea</span>
+																		<img src="/images/mu.png" alt="">
+																</div>
+															</td>
+														</tr>
+													</tbody>
+												</table>
+                    </li>
+										<li>
+                    		<table class="table">
+													<thead>
+														<tr>
+															<td colspan="2" class="p-0">
+																	<div class="header-title">
+																		<h6>Sunday 14th August 2022</h6>
+																	</div>
+															</td>
+														</tr>
+													</thead>
+													<tbody class="tbody-match">
+														<tr>
+															<td>02:00</td>
+															<td>Finished</td>
+															<td>
+																<div class="bottom-premier">
+																		<img src="/images/premier-league.png" alt="">
+																		Premier League
+																</div>
+															</td>
+															<td>
+																<div class="bottom-premier-item">
+																	<span>Liverpool</span>
+																		<img src="/images/mu.png" alt="">
+																</div>
+															</td>
+															<th>
+																5 : 0
+															</th>
+															<td>
+																<div class="bottom-premier-item">
+																		<span>Chelsea</span>
+																		<img src="/images/mu.png" alt="">
+																</div>
+															</td>
+														</tr>
+														<tr class="live">
+															<td>02:00</td>
+															<td>Live</td>
+															<td>
+																<div class="bottom-premier">
+																		<img src="/images/premier-league.png" alt="">
+																		Premier League
+																</div>
+															</td>
+															<td>
+																<div class="bottom-premier-item">
+																	<span>Liverpool</span>
+																		<img src="/images/mu.png" alt="">
+																</div>
+															</td>
+															<th>
+																5 : 0
+															</th>
+															<td>
+																<div class="bottom-premier-item">
+																		<span>Chelsea</span>
+																		<img src="/images/mu.png" alt="">
+																</div>
+															</td>
+														</tr>
+														<tr class="live">
+															<td>02:00</td>
+															<td>Live</td>
+															<td>
+																<div class="bottom-premier">
+																		<img src="/images/premier-league.png" alt="">
+																		Premier League
+																</div>
+															</td>
+															<td>
+																<div class="bottom-premier-item">
+																	<span>Liverpool</span>
+																		<img src="/images/mu.png" alt="">
+																</div>
+															</td>
+															<th>
+																5 : 0
+															</th>
+															<td>
+																<div class="bottom-premier-item">
+																		<span>Chelsea</span>
+																		<img src="/images/mu.png" alt="">
+																</div>
+															</td>
+														</tr>
+														<tr class="upcoming">
+															<td>02:00</td>
+															<td>Upcoming</td>
+															<td>
+																<div class="bottom-premier">
+																		<img src="/images/premier-league.png" alt="">
+																		Premier League
+																</div>
+															</td>
+															<td>
+																<div class="bottom-premier-item">
+																	<span>Liverpool</span>
+																		<img src="/images/mu.png" alt="">
+																</div>
+															</td>
+															<th>
+																5 : 0
+															</th>
+															<td>
+																<div class="bottom-premier-item">
+																		<span>Chelsea</span>
+																		<img src="/images/mu.png" alt="">
+																</div>
+															</td>
+														</tr>
+													</tbody>
+												</table>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+  </div>
+
+  <?php include "templates/footer.php"; ?>
+</body>
+</html>
