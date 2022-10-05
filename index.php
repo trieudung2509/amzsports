3baad3bd2805f9e47b00c211e4503d72e6a36dd3
DangKhoa96 <khoanguyenptit96@gmail.com>
2022-10-05 07:26:40 +0700

    Fix ui tabs

--- index.php
+++ index.php
@@ -761,19 +761,6 @@
 										qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem
 										eum fugiat quo voluptas nulla pariatur?</p>
 								</div>
-								<div class="block">
-									<h3 class="content-title">What ‘s AMZSport?</h3>
-									<p class="content-description">Sed ut perspiciatis unde omnis iste natus error
-										sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
-										veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-										Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
-										aut fugit, sed quia consequuntur magni dolores eos qui ratione v
-										oluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem
-										ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut
-										labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit
-										qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem
-										eum fugiat quo voluptas nulla pariatur?</p>
-								</div>
 							</div>
 						</div>
 						<div id="tabs-2" class="content-item">
@@ -782,27 +769,7 @@
 									<h3 class="content-title">What ‘s AMZSport? 02</h3>
 									<p class="content-description">Sed ut perspiciatis unde omnis iste natus error
 										sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
-										veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-										Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
-										aut fugit, sed quia consequuntur magni dolores eos qui ratione v
-										oluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem
-										ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut
-										labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit
-										qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem
-										eum fugiat quo voluptas nulla pariatur?</p>
-								</div>
-								<div class="block">
-									<h3 class="content-title">What ‘s AMZSport?</h3>
-									<p class="content-description">Sed ut perspiciatis unde omnis iste natus error
-										sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
-										veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-										Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
-										aut fugit, sed quia consequuntur magni dolores eos qui ratione v
-										oluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem
-										ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut
-										labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit
-										qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem
-										eum fugiat quo voluptas nulla pariatur?</p>
+									</p>
 								</div>
 								<div class="block">
 									<h3 class="content-title">What ‘s AMZSport?</h3>
