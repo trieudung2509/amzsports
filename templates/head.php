efbf5faf2ad74e67cf262b81e08407dea846fa63
DangKhoa96 <khoanguyenptit96@gmail.com>
2022-10-16 21:36:53 +0700

    create price page

--- templates/head.php
+++ templates/head.php
@@ -17,6 +17,7 @@
 		<link rel="stylesheet" type="text/css" href="css/news.css">
 		<link rel="stylesheet" type="text/css" href="css/news-detail.css">
 		<link rel="stylesheet" type="text/css" href="css/footer.css">
+		<link rel="stylesheet" type="text/css" href="css/price.css">
 
 		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries-->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file://-->
